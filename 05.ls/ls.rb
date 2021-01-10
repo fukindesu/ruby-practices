b63286abcad6bb6f541343ffe42475da3c845090
fukindesu <63178773+fukindesu@users.noreply.github.com>
2021-01-10 15:20:10 +0900

    mainメソッドを中心とした設計に変更（実装方法の大幅な見直し）

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -8,101 +8,92 @@ require 'etc'
 COLUMN_SIZE = 3
 
 def main
-  opts = validated_argv_opts
-  specified_path = validated_specified_path
-  paths, stats = create_paths_and_stats(specified_path, opts)
-  puts ls(specified_path, paths, stats, opts)
+  argv_path, argv_opts = fetch_cli_arguments(ARGV)
+  pathnames = create_pathnames(argv_path, argv_opts)
+  puts display_list(pathnames, argv_path, argv_opts)
 end
 
-def validated_argv_opts
-  ARGV.getopts('alr')
-rescue OptionParser::ParseError
-  puts '[ERROR] 対応できないオプション名が含まれていました'
+def fetch_cli_arguments(argv)
+  begin
+    argv_opts = argv.getopts('alr')
+  rescue OptionParser::InvalidOption # 理想はrescueを使わずに済む処理
+    exit_after_outputting('対応できないオプションが含まれていました')
+  end
+
+  if argv.empty?
+    argv_path = Pathname.new(Dir.getwd)
+  elsif argv.one? && FileTest.exist?(argv)
+    argv_path = Pathname.new(argv)
+  elsif argv.one?
+    exit_after_outputting('指定されたパスが見つかりませんでした')
+  elsif argv.size > 1
+    exit_after_outputting('パスの指定は1つだけでお願いします')
+  end
+  [argv_path, argv_opts]
+end
+
+def exit_after_outputting(message)
+  puts "[ERROR] #{message}"
   exit
 end
 
-def validated_specified_path
-  case ARGV.size
-  when 0
-    Pathname.new(Dir.getwd)
-  when 1
-    if FileTest.exist?(ARGV[0])
-      Pathname.new(ARGV[0])
-    else
-      puts '[ERROR] 指定されたパスが見つかりませんでした'
-      exit
+def create_pathnames(argv_path, argv_opts)
+  if argv_path.directory?
+    unsorted_pathnames = []
+    Dir.foreach(argv_path) do |filename|
+      next if !argv_opts['a'] && filename.start_with?('.')
+
+      pathname = Pathname.new(File.join(argv_path, filename))
+      unsorted_pathnames << pathname
     end
+    pathnames = if argv_opts['r']
+                  unsorted_pathnames.sort.reverse
+                else
+                  unsorted_pathnames.sort
+                end
+  elsif argv_path.file?
+    pathnames = [argv_path]
+  end
+  pathnames
+end
+
+def display_list(pathnames, argv_path, argv_opts)
+  if argv_opts['l']
+    display_list_with_l_opt(pathnames, argv_path)
   else
-    puts '[ERROR] パスの指定は1つだけでお願いします'
-    exit
+    display_list_without_l_opt(pathnames, argv_path)
   end
 end
 
-def create_paths_and_stats(specified_path, opts)
-  paths = []
-  stats = { name_length_max: 0, blocks_total: 0, specified_path_text: '' }
-  if specified_path.directory?
-    Dir.foreach(specified_path) do |filename|
-      next if !opts['a'] && filename.start_with?('.')
-
-      path = Pathname.new(File.join(specified_path, filename))
-      paths << path
-      stats[:name_length_max] = [stats[:name_length_max], filename.length].max
-      stats[:blocks_total] += path.stat.blocks
-    end
-    opts['r'] ? paths.sort!.reverse! : paths.sort!
-  elsif specified_path.file?
-    paths << specified_path
-    stats[:specified_path_text] = ARGV[0]
-  end
-  [paths, stats]
-end
-
-def ls(specified_path, paths, stats, opts)
-  if opts['l']
-    ls_with_l(specified_path, paths, stats)
-  else
-    ls_without_l(specified_path, paths, stats)
-  end
-end
-
-def ls_with_l(specified_path, paths, stats)
-  lines = []
-  lines << "total #{stats[:blocks_total]}" if specified_path.directory?
-  paths.each do |path|
-    line = [
-      ftype_to_chr(path.stat) + mode_to_rwx_trio(path.stat),
-      path.stat.nlink.to_s,
-      Etc.getpwuid(path.stat.uid).name,
-      Etc.getgrgid(path.stat.gid).name,
-      path.stat.size.to_s,
-      path.stat.mtime.strftime('%-m %-d %H:%M'),
-      name = if specified_path.directory?
-               path.basename.to_s
-             else
-               stats[:specified_path_text]
-             end
+def display_list_with_l_opt(pathnames, argv_path)
+  rows = []
+  rows << "total #{calc_blocks_total(pathnames)}" if argv_path.directory?
+  pathnames.each do |pathname|
+    stat = pathname.stat
+    row = [
+      ftype_to_chr(stat) + mode_to_rwx_trio(stat),
+      stat.nlink.to_s,
+      Etc.getpwuid(stat.uid).name,
+      Etc.getgrgid(stat.gid).name,
+      stat.size.to_s,
+      stat.mtime.strftime('%-m %-d %H:%M'),
+      name_for_display(pathname, argv_path)
     ].join(' ')
-    lines << line
+    rows << row
   end
-  lines
+  rows
 end
 
-def ls_without_l(specified_path, paths, stats)
-  lines = []
-  if specified_path.directory?
-    required_row_size = (paths.size.to_f / COLUMN_SIZE).ceil
-    containers = Array.new(COLUMN_SIZE) { [] }
-    paths.each_with_index do |path, idx|
-      name = path.basename.to_s.ljust(stats[:name_length_max])
-      assigned_idx = idx / required_row_size
-      containers[assigned_idx] << name
-    end
-    containers.shift.zip(*containers) { |names| lines << names.join("\t") }
-  elsif specified_path.file?
-    lines << stats[:specified_path_text]
+def calc_blocks_total(pathnames)
+  pathnames.map { |pathname| pathname.stat.blocks }.sum
+end
+
+def name_for_display(pathname, argv_path)
+  if argv_path.directory?
+    pathname.basename.to_s
+  elsif argv_path.file?
+    ARGV[0]
   end
-  lines
 end
 
 def ftype_to_chr(stat)
@@ -124,6 +115,28 @@ def mode_to_rwx_trio(stat)
   g = octal[-2].to_i
   o = octal[-1].to_i
   [formats[u], formats[g], formats[o]].join
+end
+
+def display_list_without_l_opt(pathnames, argv_path)
+  rows = []
+  if argv_path.directory?
+    containers = Array.new(COLUMN_SIZE) { [] }
+    row_size = (pathnames.size.to_f / COLUMN_SIZE).ceil
+    pathnames.each_with_index do |pathname, idx|
+      name_length_max = calc_name_length_max(pathnames)
+      name = pathname.basename.to_s.ljust(name_length_max)
+      assigned_idx = idx / row_size
+      containers[assigned_idx] << name
+    end
+    containers.shift.zip(*containers) { |names| rows << names.join("\t") }
+  elsif argv_path.file?
+    rows << display_name(pathname, argv_path)
+  end
+  rows
+end
+
+def calc_name_length_max(pathnames)
+  pathnames.map { |pathname| pathname.basename.length }.max
 end
 
 main
